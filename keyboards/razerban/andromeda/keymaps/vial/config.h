663d620ab0c06f43b96838a68bcdfd8e228985b1
Abdelkafi Ahmed <abdelkafiahmed@yahoo.fr>
2022-09-17 14:13:08 +0200

    Added Andromeda ISO

---
+++ keyboards/razerban/andromeda/keymaps/vial/config.h
@@ -0,0 +1,23 @@
+/*
+Copyright 2022 Razerban
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/* VIA layer count */
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
+
+/* VIAL Keyboard UID */
+#define VIAL_KEYBOARD_UID {0x10, 0x7B, 0x46, 0x07, 0x04, 0x06, 0x76, 0xF0}
+
+/* VIAL Unlock combo */
+#define VIAL_UNLOCK_COMBO_ROWS {0, 3}
+#define VIAL_UNLOCK_COMBO_COLS {0, 12}
