625787bd45d80d8d8d464f694e9b7af5af5cfe66
Abdelkafi Ahmed <abdelkafiahmed@yahoo.fr>
2023-02-01 10:30:11 +0100

    Reformat printed text

--- build_firmwares.sh
+++ build_firmwares.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 echo "############################################"
-echo "####     Updating submodules            ####"
+echo "#### Updating submodules                ####"
 echo -e "############################################\n"
 make git-submodule >/dev/null 2>&1
 
 echo "############################################"
-echo "####     Building Andromeda firmware    ####"
+echo "#### Building Andromeda firmware        ####"
 echo "############################################"
 make clean >/dev/null 2>&1
-echo "#        Building default firmware         #"
+echo "#    Building default firmware             #"
 if ! make razerban/andromeda:default 1>/dev/null;
 then
   exit 1
 fi
 make clean >/dev/null 2>&1
-echo "#        Building VIAL firmware            #"
+echo "#    Building VIAL firmware                #"
 if ! make razerban/andromeda:vial 1>/dev/null;
 then
   exit 1
